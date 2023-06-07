3af30a0a76a3b11468889ceceb6a5e51a2e44237
VinokurovDV <vinokurov2002@list.ru>
2023-06-07 12:58:25 +0300

    Добавлены проверки на корректность входных данных и возврат -1, если входные данные некорректны

--- Figure.cs
+++ Figure.cs
@@ -7,41 +7,49 @@ namespace FigureSquare
     {
         public virtual double CalculateSquare(double r)
         {
-            return Math.PI * Math.Pow(r,2);
+            if (r > 0) return Math.PI * Math.Pow(r, 2);
+            else return -1;
         }
         public double CalculateSquare(double a, double b, double c)
         {
-            //если треугольник прямоугольный (проверяем по т. Пифагора), то его площадь равна половине произведения катетов
-            if(a>b)
+            if (a > 0 && b > 0 && c > 0)
             {
-                if (a > c) 
-                {
-                    
-                    if (a*a==b*b+c*c)
-                        return b*c/2;
-                }
-                else
-                {
-                    if(c*c==b*b+a*a)
-                        return a*b/2;
-                }
+                
+                    //если треугольник прямоугольный (проверяем по т. Пифагора), то его площадь равна половине произведения катетов
+                    if (a > b)
+                    {
+                        if (a > c)
+                        {
+
+                            if (a * a == b * b + c * c)
+                                return b * c / 2;
+                        }
+                        else
+                        {
+                            if (c * c == b * b + a * a)
+                                return a * b / 2;
+                        }
+                    }
+                    else
+                    {
+                        if (b > c)
+                        {
+                            if (b * b == a * a + c * c)
+                                return a * c / 2;
+                        }
+                        else
+                        {
+                            if (c * c == b * b + a * a)
+                                return a * b / 2;
+                        }
+                    }
+                    //иначе площадь равна корню из произведения полупериметра на на разности полупериметра и каждой из его сторон
+                    double pp = (a + b + c) / 2;
+                    double s = Math.Sqrt(pp * (pp - a) * (pp - b) * (pp - c));
+                    return s;
+             
             }
-            else
-            {
-                if (b>c)
-                { if (b * b == a * a + c * c)
-                        return a * c / 2;
-                 }
-                else 
-                {
-                    if (c * c == b * b + a * a)
-                            return a * b / 2;
-                }
-            }
-            //иначе площадь равна корню из произведения полупериметра на на разности полупериметра и каждой из его сторон
-            double pp =(a+b+c)/2;
-            double s=Math.Sqrt(pp*(pp-a)*(pp-b)*(pp-c));
-            return s;
+            return -1;
         }
     }
 }
